839d5e1a1ee11f0a030117d51ada5dfe053c7587
hekejian <fei.94625@qq.com>
2015-10-13 20:10:12 +0800

    Test

---
+++ Learning.js
@@ -0,0 +1,30 @@
+//这是第一条代码
+
+if (2>1) {
+	x = 3.34e4;
+	y = 0xff00;
+	z = 3;
+	if (x < y){    
+		z = 4;
+	}
+	if(x>y){
+		z = 5;
+	}
+}
+console.log(x,y,z);
+var age = 15;
+if (age > 18){
+	alert("adult");
+}else{
+	alert("teenager");
+}
+var person ={
+	name: 'kejian',
+	age: 21,
+	tages: ['js','web','mobile'],
+	city:'shenzhen',
+	zipcode:null
+
+};
+console.log(person.name);
+console.log(person.tages);
